7370ec3654c07722bb8a989dfe7790b8d30faf82
Marqae Morris <mamorris920@gmail.com>
2024-09-28 22:38:04 -0400

    completed assignment

---
+++ main.js
@@ -0,0 +1,74 @@
+document.querySelector('#min').addEventListener('click', min)
+document.querySelector('#max').addEventListener('click', max)
+//set a starting point(wallet balance)
+let walletBalance = 500
+document.querySelector('#wallet').innerText = walletBalance
+console.log(walletBalance)
+//Event Listener
+
+function min(){
+//slotA
+let slotA = Math.floor(Math.random() * 3)
+console.log(slotA)
+document.querySelector('#slotA').innerText = slotA
+
+// slotB 
+let slotB = Math.floor(Math.random() * 3)
+console.log(slotB)
+document.querySelector('#slotB').innerText = slotB
+
+// slotC 
+let slotC= Math.floor(Math.random() * 3)
+console.log(slotC)
+document.querySelector('#slotC').innerText = slotC
+
+// if all slots have the same number- player wins
+if(slotA === slotB && slotA === slotC){
+    console.log('true')
+    walletBalance += 10 //shorthand walletBalance + 10
+    document.querySelector('#wallet').innerText = walletBalance
+    document.querySelector('#winLose').innerText = 'You win!!'
+
+// if they dont match display the balance and they lose
+}
+else{
+    console.log('false')
+    walletBalance -= 10 //shorthand walletBalance + 10
+    document.querySelector('#wallet').innerText = walletBalance
+    document.querySelector('#winLose').innerText = 'You Suck, try again'    
+
+}
+}
+function max(){
+    //slotA
+    let slotA = Math.floor(Math.random() * 5)
+    console.log(slotA)
+    document.querySelector('#slotA').innerText = slotA
+    
+    // slotB 
+    let slotB = Math.floor(Math.random() * 5)
+    console.log(slotB)
+    document.querySelector('#slotB').innerText = slotB
+    
+    // slotC 
+    let slotC= Math.floor(Math.random() * 5)
+    console.log(slotC)
+    document.querySelector('#slotC').innerText = slotC
+    
+    // if all slots have the same number- player wins
+    if(slotA === slotB && slotA === slotC){
+        console.log('true')
+        walletBalance += 100 //shorthand walletBalance + 100
+        document.querySelector('#wallet').innerText = walletBalance
+        document.querySelector('#winLose').innerText = 'You win!!'
+    
+    // if they dont match display the balance and they lose
+    }
+    else{
+        console.log('false')
+        walletBalance -= 100 //shorthand walletBalance + 100
+        document.querySelector('#wallet').innerText = walletBalance
+        document.querySelector('#winLose').innerText = 'You Suck, try again'    
+    
+    }
+    }
